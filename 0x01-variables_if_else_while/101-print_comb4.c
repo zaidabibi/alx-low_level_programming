19bac73eac642afc9d85183ebaea4336120f8bb8
zaida <zaidabibi.zee1.mma@gmail.com>
2022-06-06 20:09:53 +0200

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,73 +1,44 @@
 #include <stdio.h>
 
-
-
 /**
+ * main - Prints numbers between 012 to 789.
  *
- *  * main - Prints numbers between 00 to 89.
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
+	int i, e, g;
 
-		int i, e;
+	i = 48;
+	e = 48;
+	g = 48;
 
-
-
-			i = 48;
-
-				e = 48;
-
-
-
-					while (e < 58)
-
-							{
-
-										i = 48;
-
-												while (i < 58)
-
-															{
-
-																			if (e != i && e < i)
-
-																							{
-
-																												putchar(e);
-
-																																putchar(i);
-
-																																				if (i == 57 && e == 56)
-
-																																									{
-
-																																															break;
-
-																																																			}
-
-																																								putchar(',');
-
-																																												putchar(' ');
-
-																																															}
-
-																						i++;
-
-																								}
-
-														e++;
-
-															}
-
-						putchar('\n');
-
-							return (0);
-
+	while (e < 58)
+	{
+		i = 48;
+		while (i < 58)
+		{
+			g = 48;
+			while (g < 58)
+			{
+				if (e != i && e != g && i != g && e < i && i < g)
+				{
+					putchar(e);
+					putchar(i);
+					putchar(g);
+					if (i == 56 && e == 55 && g == 57)
+					{
+						break;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+				g++;
+			}
+			i++;
+		}
+		e++;
+	}
+	putchar('\n');
+	return (0);
 }
