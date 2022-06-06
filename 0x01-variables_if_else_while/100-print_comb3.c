8fafc0bce21ddea42f4a8d34934acd1af963b696
zaida <zaidabibi.zee1.mma@gmail.com>
2022-06-06 20:01:29 +0200

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,45 +1,37 @@
 #include <stdio.h>
 
-
-
 /**
+ * main - Prints numbers between 00 to 89.
  *
- *  * main - Prints numbers between 0 to 9 with commas and spaces.
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
+	int i, e;
 
-		int i;
+	i = 48;
+	e = 48;
 
-
-
-			for (i = 48; i < 58; i++)
-
-					{
-
-								putchar(i);
-
-										if (i != 57)
-
-													{
-
-																	putchar(',');
-
-																				putchar(' ');
-
-																						}
-
-											}
-
-				putchar('\n');
-
-					return (0);
-
+	while (e < 58)
+	{
+		i = 48;
+		while (i < 58)
+		{
+			if (e != i && e < i)
+			{
+				putchar(e);
+				putchar(i);
+				if (i == 57 && e == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			i++;
+		}
+		e++;
+	}
+	putchar('\n');
+	return (0);
 }
