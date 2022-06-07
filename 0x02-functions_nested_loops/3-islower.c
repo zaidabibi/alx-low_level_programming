f77f8b198dcd8894aa78ddefc1efeff382003c9b
zaida <zaidabibi.zee1.mma@gmail.com>
2022-06-08 00:31:04 +0200

    Update 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - check for lowercase character
+ *
+ * Return: 1 is successful
+ * otherwise return 0
+ *
+ * @c: input
+ */
+int _islower(int c)
+{
+	if (islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
