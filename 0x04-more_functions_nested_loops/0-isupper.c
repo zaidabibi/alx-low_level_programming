1cc1813dc2116f7e572741825e5ba57e3bc4489d
zaida <zaidabibi.zee1.mma@gmail.com>
2022-06-09 21:25:19 +0200

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,1 +1,18 @@
-jj
+#include <ctype.h>
+/**
+ * _isupper - return 1 if c is uppercase and 0 if not
+ * @c: input
+ * Return: int
+ */
+
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+	return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
