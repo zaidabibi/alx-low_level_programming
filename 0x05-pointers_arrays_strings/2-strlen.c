7209cc7dfdf20c65a65cbc83577f5e41a174dced
zaida <zaidabibi.zee1.mma@gmail.com>
2022-06-13 07:41:03 +0200

    Create 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - writes the character c to stdout
+ * @s: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _strlen(char *s)
+{
+int y;
+for (y = 0; *s != '\0'; s++)
+{
+y++;
+}
+return (y);
+}
