e3285edd7a32caac6490caa41b9ac6e0ae7cc5c4
zaida <zaidabibi.zee1.mma@gmail.com>
2022-06-13 07:38:11 +0200

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,1 +1,12 @@
-jj
+#include "main.h"
+/**
+ * reset_to_98 - writes the character c to stdout
+ * @n: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
