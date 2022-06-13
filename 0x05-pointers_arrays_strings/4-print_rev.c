8e51bcf2adba1a105ca916235b4e3facc6e2737b
zaida <zaidabibi.zee1.mma@gmail.com>
2022-06-13 20:56:50 +0200

    task submission

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string in reverse
+ * @s: string to be printed
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
