e481c298e9245b5eff9eb334438c2a9b7141d674
zaida <zaidabibi.zee1.mma@gmail.com>
2022-06-13 20:53:59 +0200

    task submission

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,20 @@
-jj
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: string to be printed
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
