7ca7e6f8214b6f33684b2bb3fad9c9aafa8a26ce
zaida <zaidabibi.zee1.mma@gmail.com>
2022-06-16 19:55:15 +0200

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,1 +1,21 @@
-jjj
+#include "main.h"
+
+/**
+ * string_toupper - Function prototype
+ * Description: Converts a string ro uppercase
+ * @str: The string to be converted to uppercase
+ * Return: Pointer to the str
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+			str[i] -= 32;
+	}
+
+	return (str);
+}
