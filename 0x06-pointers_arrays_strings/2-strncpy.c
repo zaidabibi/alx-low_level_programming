e29920357fb5dabe97a4133aed000f69a1f33bf6
zaida <zaidabibi.zee1.mma@gmail.com>
2022-06-16 19:36:49 +0200

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,1 +1,27 @@
-hhh
+#include "main.h"
+
+/**
+ * _strncpy - Function prototype
+ * Description: Copies the first n characters of the string pointed to by src,
+ * including the terminating null byte ('\0'), to the buffer pointed to by dest
+ * @dest: Pointer to the destination string
+ * @src: Pointer to the string to be copied
+ * @n: Number of bytes to copy
+ * Return: The pointer to the destination string.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i]; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
+}
