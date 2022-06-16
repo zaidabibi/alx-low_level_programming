c6ca6f7d1147a5dbb51f20ffe0267e9194c3c6a1
zaida <zaidabibi.zee1.mma@gmail.com>
2022-06-16 20:06:36 +0200

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,1 +1,32 @@
-nnn
+#include "main.h"
+
+/**
+ * leet - Function prototype
+ * Description: Encodes a string into 1337, replaces all the letters
+ * a, e, o, t, and l with the numbers 4, 3, 0, 7, and 1 respectively
+ * @str: The string to encode
+ * Return: Pointer to the str
+ */
+char *leet(char *str)
+{
+	int i;
+	char lower[] = "aeotl";
+	char upper[] = "AEOTL";
+	char num[] = "43071";
+
+	for (i = 0; str[i]; i++)
+	{
+		int x;
+
+		for (x = 0; x < 5; x++)
+		{
+			if (str[i] != lower[x] && str[i] != upper[x])
+				continue;
+
+			str[i] = num[x];
+			break;
+		}
+	}
+
+	return (str);
+}
