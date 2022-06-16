a2b4c714044d6da48c3663d502aa491b3b4fae21
zaida <zaidabibi.zee1.mma@gmail.com>
2022-06-16 20:01:37 +0200

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,1 +1,44 @@
-jjj
+#include "main.h"
+
+/**
+ * cap_string - Function prototype
+ * Description: Capitalizes all words of a string. If the current character is
+ * a letter, and the previous character is a separator, then capitalize the
+ * current character
+ * @str: The string to capitalize
+ * Return: Pointer to the str
+ */
+char *cap_string(char *str)
+{
+	char sep[] = ",\t;\n; .!?\"(){}";
+
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		int x = 0;
+
+		if (i == 0)
+			x = 1;
+		else
+		{
+			int y;
+
+			for (y = 0; sep[y]; y++)
+			{
+				if (str[i - 1] != sep[y])
+					continue;
+				x = 1;
+				break;
+			}
+		}
+
+		if (x != 1)
+			continue;
+
+		if (str[i] <= 'z' && str[i] >= 'a')
+			str[i] -= ('a' - 'A');
+	}
+
+	return (str);
+}
