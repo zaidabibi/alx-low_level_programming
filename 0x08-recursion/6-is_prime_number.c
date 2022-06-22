1c8718b1cc98adcc41591ba571387ddd1590d587
zaida <zaidabibi.zee1.mma@gmail.com>
2022-06-22 21:17:47 +0200

    task submission

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,32 +1,41 @@
 #include "main.h"
-/**
-* is_prime_number - returns true if the number is prime
-*@n: the number to check
-*
-*Return: true if the number is prime
-*/
-int is_prime_number(int n)
-{
-	int start = n / 2;
 
-	if (n <= 1)
-		return (0);
-	return (is_prime(n, start));
+/**
+ *check - primer number for
+ *@n: par
+ *@i: par
+ *Return: function
+ */
+
+int check(int n, int i)
+{
+	if (i == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		if (n % i == 0)
+		{
+			return (0);
+		}
+		else
+		{
+			return (check(n, i - 1));
+		}
+	}
 }
 
 /**
-* is_prime - returns the 1 if n is prime
-* @n: number to be checked
-* @start: number to start checking from
-*
-* Return: 1 if n is prime, 0 otherwise
-*/
+ *is_prime_number - prime
+ *@n: par
+ *Return: function
+ */
 
-int is_prime(int n, int start)
+int is_prime_number(int n)
 {
-	if (start <= 1)
-		return (1);
-	else if (n % start == 0)
+	if (n == 1)
 		return (0);
-	return (is_prime(n, start - 1));
+	else
+		return (check(n, n / 2));
 }
