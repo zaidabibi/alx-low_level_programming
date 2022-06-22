500160503fe31faaf84ef45842653dd6ab1ab41b
zaida <zaidabibi.zee1.mma@gmail.com>
2022-06-22 20:55:08 +0200

    task submission

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - writes the character n to stdout
+ * @s: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void _print_rev_recursion(char *s)
+{
+if (*s == '\0')
+	{
+	return;
+	}
+_print_rev_recursion(s + 1);
+
+_putchar(*s);
+}
