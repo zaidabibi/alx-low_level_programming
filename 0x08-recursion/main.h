7b8f911efd9f09230eea0ee524e0358669c9a8cf
zaida <zaidabibi.zee1.mma@gmail.com>
2022-06-22 20:52:22 +0200

    task submission recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,1 +1,13 @@
-jj
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int _other_sqrt(int x, int y);
+
+#endif
