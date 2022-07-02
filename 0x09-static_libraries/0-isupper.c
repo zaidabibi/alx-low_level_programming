91b83e7549239b0b8a3d90abec1cdddf57010291
zaida <zaidabibi.zee1.mma@gmail.com>
2022-07-02 19:24:20 +0200

    task submission

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,18 @@
+#include <ctype.h>
+/**
+ * _isupper - return 1 if c is uppercase and 0 if not
+ * @c: input
+ * Return: int
+ */
+
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+	return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
