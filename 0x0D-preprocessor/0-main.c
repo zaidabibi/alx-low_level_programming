cfdcd18af7936ecbd586ac2f246a9433059c8462
zaida <zaidabibi.zee1.mma@gmail.com>
2022-07-04 18:00:17 +0200

    testing code

--- 0x0D-preprocessor/0-main.c
+++ 0x0D-preprocessor/0-main.c
@@ -1,12 +1,17 @@
 #include "0-object_like_macro.h"
 #include "0-object_like_macro.h"
 #include <stdio.h>
+
 /**
- * main - checking code
+ * main - check the code.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	int s;
-	s = 98 + SIZE
+    int s;
+
+    s = 98 + SIZE;
+    printf("%d\n", s);
+    return (0);
+}
