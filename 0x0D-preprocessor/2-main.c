1559920c9c510b9302034e8627dd36a41b01a0cd
zaida <zaidabibi.zee1.mma@gmail.com>
2022-07-04 17:21:43 +0200

    2-main.c task submission

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,1 +1,14 @@
-
+#include <stdio.h>
+/**
+ *  main - writes the character n to stdout
+ *
+ *
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
