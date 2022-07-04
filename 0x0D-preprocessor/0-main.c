69114c89c4aa45877321ffd020e29018a1fa4897
zaidabibi <zidabibi.zee1.mma@gmail.com>
2022-07-04 08:58:15 -0700

    test

--- 0x0D-preprocessor/0-main.c
+++ 0x0D-preprocessor/0-main.c
@@ -1,17 +1,12 @@
 #include "0-object_like_macro.h"
 #include "0-object_like_macro.h"
 #include <stdio.h>
-
 /**
- * main - check the code for Holberton School students.
+ * main - checking code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    int s;
-
-    s = 1024 + SIZE;
-    printf("%d\n", s);
-    return (0);
-}
+	int s;
+	s = 98 + SIZE
